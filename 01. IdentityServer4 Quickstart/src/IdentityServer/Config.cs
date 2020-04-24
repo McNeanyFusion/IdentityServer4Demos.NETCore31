df36e89f9cc4333791e9157c90264b5ebe436edc
Scott McNeany <smcneany@fusionalliance.com>
2020-04-24 12:43:43 -0400

    PKCE and password flows both working with example in Postman

--- 01. IdentityServer4 Quickstart/src/IdentityServer/Config.cs
+++ 01. IdentityServer4 Quickstart/src/IdentityServer/Config.cs
@@ -2,8 +2,12 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityModel;
+using IdentityServer4;
 using IdentityServer4.Models;
+using IdentityServer4.Test;
 using System.Collections.Generic;
+using System.Security.Claims;
 
 namespace IdentityServer
 {
@@ -11,17 +15,67 @@ namespace IdentityServer
     {
         public static IEnumerable<IdentityResource> Ids =>
             new IdentityResource[]
-            { 
-                new IdentityResources.OpenId()
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile()
             };
 
         public static IEnumerable<ApiResource> Apis =>
-            new ApiResource[] 
-            { };
-        
+            new[] {
+                new ApiResource("socialnetwork", "Social Network")
+            };
+
         public static IEnumerable<Client> Clients =>
-            new Client[] 
-            { };
-        
+            new Client[]
+            {
+                new Client() {
+                     ClientId = "TrustedInternalApi",
+                    ClientSecrets = new [] { new Secret("secret".Sha256()) },
+                     AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowedScopes = new [] {
+                        IdentityServerConstants.StandardScopes.OpenId,
+        IdentityServerConstants.StandardScopes.Profile,
+        IdentityServerConstants.StandardScopes.Email,
+                        "socialnetwork" }
+                },
+                new Client() {
+                     ClientId = "UntrustedExternalApi",
+                     ClientName = "Untrusted External API",
+                    ClientSecrets = new [] { new Secret("secret".Sha256()) },
+                     AllowedGrantTypes = GrantTypes.Code,
+                     RequireConsent = true,
+                AllowRememberConsent = false,
+                AllowAccessTokensViaBrowser = true,
+                RequirePkce = true,
+                RedirectUris =
+                  new List<string> {
+                       "http://localhost:5436/account/oAuth2", "https://localhost:44339/"
+                  },
+                PostLogoutRedirectUris =
+                  new List<string> {"http://localhost:5436"},
+                    AllowedScopes = new [] { "socialnetwork" },
+                    AccessTokenType = AccessTokenType.Jwt
+
+                }
+            };
+
+        public static IEnumerable<TestUser> Users()
+        {
+            return new[] {
+                new TestUser{SubjectId = "818727", Username = "alice", Password = "alice",
+                Claims =
+                {
+                    new Claim(JwtClaimTypes.Name, "Alice Smith"),
+                    new Claim(JwtClaimTypes.GivenName, "Alice"),
+                    new Claim(JwtClaimTypes.FamilyName, "Smith"),
+                    new Claim(JwtClaimTypes.Email, "AliceSmith@email.com"),
+                    new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                    new Claim(JwtClaimTypes.WebSite, "http://alice.com"),
+                    new Claim(JwtClaimTypes.Address, @"{ 'street_address': 'One Hacker Way', 'locality': 'Heidelberg', 'postal_code': 69118, 'country': 'Germany' }", IdentityServer4.IdentityServerConstants.ClaimValueTypes.Json)
+                }
+            }
+            };
+        }
     }
 }
--- 01. IdentityServer4 Quickstart/src/IdentityServer/Startup.cs
+++ 01. IdentityServer4 Quickstart/src/IdentityServer/Startup.cs
@@ -2,16 +2,24 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
+using IdentityServer4.Services;
+using IdentityServer4.Test;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace IdentityServer
 {
     public class Startup
     {
         public IWebHostEnvironment Environment { get; }
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
         public Startup(IWebHostEnvironment environment)
         {
@@ -23,10 +31,33 @@ namespace IdentityServer
             // uncomment, if you want to add an MVC-based UI
             services.AddControllersWithViews();
 
-            var builder = services.AddIdentityServer()
+            //services.AddCors(options =>
+            //{
+            //    options.AddPolicy("CorsPolicy",
+            //        builder => builder.WithOrigins("https://localhost:44339")
+            //        .AllowAnyMethod()
+            //        .AllowAnyHeader());
+            //});
+
+
+            //var cors = new DefaultCorsPolicyService(null)
+            //{
+            //    AllowedOrigins = { "https://localhost:44339" }
+            //};
+            //services.AddSingleton<ICorsPolicyService>(cors);
+
+
+            var builder = services.AddIdentityServer(opts =>
+            {
+                opts.UserInteraction.LoginUrl = "/Account/Login";
+                opts.UserInteraction.LogoutUrl = "/Account/Logout";
+            })
+                //.AddSigningCredential(new X509Certificate2(@"C:\Users\smcneany\Downloads\asp-dot-net-core-oauth\03\demos\SocialNetwork\socialnetwork.pfx", "password"))
                 .AddInMemoryIdentityResources(Config.Ids)
                 .AddInMemoryApiResources(Config.Apis)
-                .AddInMemoryClients(Config.Clients);
+                .AddInMemoryClients(Config.Clients)
+                .AddTestUsers(Config.Users().ToList());
+
 
             // not recommended for production - you need to store your key material somewhere secure
             builder.AddDeveloperSigningCredential();
@@ -38,12 +69,23 @@ namespace IdentityServer
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
 
             // uncomment if you want to add MVC
             app.UseStaticFiles();
             app.UseRouting();
 
+
+
+            app.UseCors("CorsPolicy");
             app.UseIdentityServer();
+
 
             // uncomment, if you want to add MVC
             app.UseAuthorization();
